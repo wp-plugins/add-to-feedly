b2d3ff4e6f7c136cacbe3a63ca7db96c8a42b745
davidmerinas <davidmerinas@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-08-06 16:42:57 +0000

    New version and new feature floating "add to Feedly" button.
    git-svn-id: https://plugins.svn.wordpress.org/add-to-feedly/trunk@752385 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- addtofeedly.php
+++ addtofeedly.php
@@ -2,18 +2,90 @@
 /*
 Plugin Name: Add to feedly
 Plugin URI: http://wordpress.org/plugins/add-to-feedly/
-Description: Feedly users can subscribe your RSS feed just by clicking the banner "Follow on Feedly" that this plugin provides. You can show the banner in English or Spanish. Los usuarios de Feedly pueden suscribirse facilmente a tus RSS con tan solo hacer clic sobre el banner "Sigueme en Feedly" que proporciona este plugin. El banner esta disponible en Ingles y Español. 
-Version: 1.0.1
+Description: Feedly users can subscribe your RSS feed just by clicking the banner "Follow on Feedly" or the floating button that this plugin provides. You can show the banner in English or Spanish. Los usuarios de Feedly pueden suscribirse facilmente a tus RSS con tan solo hacer clic sobre el banner "Sigueme en Feedly" o en el boton flotante que proporciona este plugin. El banner esta disponible en Ingles y Espanol. 
+Version: 1.1
 Author: davidmerinas
 Author URI: http://www.davidmerinas.com
 */
 
 define(ADD_TO_FEEDLY_WIDGET_ID, "widget_ADD_TO_FEEDLY");
 
+function ADD_TO_FEEDLY_create_menu() {
+
+	//create new top-level menu
+	add_menu_page('Add to Feedly Plugin', 'Add to Feedly', 'administrator', __FILE__, 'ADD_TO_FEEDLY_settings_page',plugins_url('/images/icon.png', __FILE__));
+
+	//call register settings function
+	add_action( 'admin_init', 'ADD_TO_FEEDLY_register_mysettings' );
+}
+
+function ADD_TO_FEEDLY_register_mysettings() {
+	//register our settings
+	register_setting( 'ADD_TO_FEEDLY-settings-group', 'ADD_TO_FEEDLY_active' );
+	register_setting( 'ADD_TO_FEEDLY-settings-group', 'ADD_TO_FEEDLY_feed_url' );
+	register_setting( 'ADD_TO_FEEDLY-settings-group', 'ADD_TO_FEEDLY_position' );
+	register_setting( 'ADD_TO_FEEDLY-settings-group', 'ADD_TO_FEEDLY_size' );
+}
+
+// Add settings link on plugin page
+function addtofeedly_settings_link($links) { 
+  $settings_link = '<a href="options-general.php?page=add-to-feedly/addtofeedly.php">Settings</a>'; 
+  array_unshift($links, $settings_link); 
+  return $links; 
+}
+ 
+
+function ADD_TO_FEEDLY_settings_page() {
+?>
+<div class="wrap">
+<h2>Add to Feedly</h2>
+
+<form method="post" action="options.php">
+    <?php settings_fields( 'ADD_TO_FEEDLY-settings-group' ); ?>
+    <?php do_settings_sections( 'ADD_TO_FEEDLY-settings-group' ); ?>
+    <table class="form-table">
+		<tr style="width:420px" valign="top">
+			<th scope="row"><?php _e('Active','addtofeedly');?> Floating Add to Feedly</th>
+			<td><input type="checkbox" name="ADD_TO_FEEDLY_active" <?php echo get_option('ADD_TO_FEEDLY_active')?'checked="checked"':''; ?>/></td>
+        </tr>
+		
+        <tr style="width:420px" valign="top">
+        <th scope="row">Feed URL (http://...)</th>
+        <td><input style="width:320px" type="text" name="ADD_TO_FEEDLY_feed_url" value="<?php echo get_option('ADD_TO_FEEDLY_feed_url'); ?>" /></td>
+        </tr>
+         
+		 <tr style="width:420px" valign="top">
+        <th scope="row"><?php _e('Size','addtofeedly');?></th>
+        <td>
+			<select style="width:120px" name="ADD_TO_FEEDLY_size">
+				<option value="big" <?=get_option('ADD_TO_FEEDLY_size')=="big"?'selected="selected"':''?>><?php _e('Big','addtofeedly');?></option>
+				<option value="medium" <?=get_option('ADD_TO_FEEDLY_size')=="medium"?'selected="selected"':''?>><?php _e('Medium','addtofeedly');?></option>
+				<option value="small" <?=get_option('ADD_TO_FEEDLY_size')=="small"?'selected="selected"':''?>><?php _e('Small','addtofeedly');?></option>
+			</select>
+		</td>
+        </tr>
+		
+        <tr style="width:420px" valign="top">
+        <th scope="row"><?php _e('Position','addtofeedly');?></th>
+        <td>
+			<select style="width:120px" name="ADD_TO_FEEDLY_position">
+				<option value="left" <?=get_option('ADD_TO_FEEDLY_position')=="left"?'selected="selected"':''?>><?php _e('Left','addtofeedly');?></option>
+				<option value="right" <?=get_option('ADD_TO_FEEDLY_position')=="right"?'selected="selected"':''?>><?php _e('Right','addtofeedly');?></option>
+			</select>
+		</td>
+        </tr>
+    </table>
+    
+    <?php submit_button(); ?>
+
+</form>
+</div>
+<?}
+
 function ADD_TO_FEEDLY_showimage($feeds='http://feeds.feedburner.com/davidmerinas',$lang="es"){
-		$path=get_bloginfo('url')."/wp-content/plugins/add-to-feedly/";
+		$path=get_bloginfo('url')."/wp-content/plugins/".basename( dirname( __FILE__ ) )."/";
 		$url="http://cloud.feedly.com/#subscription%2Ffeed%2F".urlencode($feeds);
-		echo('<a id="addtofeedly" href="'.$url.'" title="'.__("Follow on Feedly").'"><img src="'.$path.'images/addtofeedly_'.$lang.'.png" alt="'.__("Follow on Feedly").'"/></a>');
+		echo('<a id="addtofeedly" href="'.$url.'" title="'.__("Follow on","addtofeedly").' Feedly" target="__blank"><img src="'.$path.'images/addtofeedly_'.$lang.'.png" alt="'.__("Follow on","addtofeedly").' Feedly"/></a>');
 }
 
 function widget_ADD_TO_FEEDLY_control() {
@@ -45,7 +117,7 @@ function widget_ADD_TO_FEEDLY_control() {
 	</p>
 	<p>
 	  <label for="<?php echo ADD_TO_FEEDLY_WIDGET_ID;?>-lang">
-	    Language - Idioma
+	    <?php _e('Language','addtofeedly');?>
 	  </label>
 	  <select class="widefat"
 	    type="text"
@@ -80,14 +152,30 @@ function widget_ADD_TO_FEEDLY_init() {
 			__('ADD_TO_FEEDLY'), 'widget_ADD_TO_FEEDLY_control');
 }
 
-function prefix_add_my_stylesheet() {
+function addtofeedly_stylesheet() {
     // Respects SSL, Style.css is relative to the current file
-    wp_register_style( 'prefix-style', plugins_url('style.css', __FILE__) );
-    wp_enqueue_style( 'prefix-style' );
+    wp_register_style( 'addtofeedly-style', plugins_url('style.css', __FILE__) );
+    wp_enqueue_style( 'addtofeedly-style' );
+}
+
+function ADD_TO_FEEDLY_init(){
+	if(get_option('ADD_TO_FEEDLY_active'))
+	{
+		$path=get_bloginfo('url')."/wp-content/plugins/".basename( dirname( __FILE__ ) )."/";
+		echo('<div id="float_feed_box" class="'.get_option('ADD_TO_FEEDLY_position').' floatfeed_'.get_option('ADD_TO_FEEDLY_size').'"><a href="http://cloud.feedly.com/#subscription%2Ffeed%2F'.urlencode(get_option('ADD_TO_FEEDLY_feed_url')).'" title="'.__('Follow on','addtofeedly').' Feedly" target="__blank"><img src="'.$path.'/images/feedly-follow-'.get_option('ADD_TO_FEEDLY_size').'.png" alt="'.__('Follow on','addtofeedly').' Feedly"/></a></div>');
+	}
 }
 
 // Registrar el widget en WordPress
-add_action('wp_enqueue_scripts', 'prefix_add_my_stylesheet');
+load_plugin_textdomain('addtofeedly', false, basename( dirname( __FILE__ ) ) . '/languages' );
+add_action('wp_enqueue_scripts', 'addtofeedly_stylesheet');
+if ( is_admin() ){
+	add_action('admin_menu', 'ADD_TO_FEEDLY_create_menu');
+	$plugin = plugin_basename(__FILE__); 
+	add_filter("plugin_action_links_$plugin", 'addtofeedly_settings_link' );
+}
 add_action("plugins_loaded", "widget_ADD_TO_FEEDLY_init");
-
+if ( !is_admin() ){
+	add_action("plugins_loaded", "ADD_TO_FEEDLY_init");
+}
 ?>
--- readme.txt
+++ readme.txt
@@ -8,37 +8,47 @@ Stable tag: trunk
 License: GPLv2 or later
 License URI: http://www.gnu.org/licenses/gpl-2.0.html
 
-This plugin provides a widget to Display a "Follow on Feedly" banner in your sidebar.
+This plugin provides a widget to Display a "Follow on Feedly" banner in your sidebar and a floating button at bottom.
 
 == Description ==
 
-Feedly users can subscribe your RSS feed just by clicking the banner "Follow on Feedly" that this plugin provides. You can show the banner in English or Spanish.
-Los usuarios de Feedly pueden suscribirse facilmente a tus RSS con tan solo hacer clic sobre el banner "Sigueme en Feedly" que proporciona este plugin. El banner esta disponible en Ingles y Espanol. 
+Feedly users can subscribe your RSS feed just by clicking the banner "Follow on Feedly" or the floating button that this plugin provides. You can show the banner in English or Spanish. Several sizes available for Floating button.
+Los usuarios de Feedly pueden suscribirse facilmente a tus RSS con tan solo hacer clic sobre el banner "Sigueme en Feedly" o el boton flotante que proporciona este plugin. El banner esta disponible en Ingles y Espanol. Disponibles varios tamanos para el boton flotante.
 
 == Installation ==
 
 1. Uncompress the file and upload the folder 'add-to-feedly' to the '/wp-content/plugins/' directory
 2. Activate the plugin through the 'Plugins' menu in WordPress
+
+To activate the widget:
 3. Go to 'Widgets' menu and drag and drop 'Add to Feedly' widget to your prefered sidebar
 4. Configure the widget: select Language and fill in the URL of your RSS Feed
-5. Clic on "Save"
+5. Click on "Save"
+
+To configure the floating button:
+6. Go to "Add to feedly" menu button and check "active" checkbox. Then fill in the URL of your RSS Feed and select the position and the size of the button .
+7. Click on "Save"
 
 == Frequently asked questions ==
 Not yet
 
 
 == Screenshots ==
-1. screenshot-1.png
+1. screenshot-button.png
+2. screenshot-1.png
 2. screenshot-2.png
 
 
 == Changelog ==
 
-= 1.0 =
-*First stable version
+= 1.1 =
+*Fixed some errors, added translations and a new feature: floating "add to Feedly" button.
 
 = 1.0.1 =
 *Fixed some CSS errors
 
+= 1.0 =
+*First stable version
+
 == Upgrade notice ==
-Nothing here
+Updating will not add automatically any unwanted feature because Floating button is inactive by default.
